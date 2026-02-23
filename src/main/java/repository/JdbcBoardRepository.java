b8b68ce603abea1bbc141922161b8e68cef81efa
bam <devbam515@gmail.com>
2026-02-23 17:03:51 +0900

    Jdbc > 작업 진행 중

---
+++ src/main/java/repository/JdbcBoardRepository.java
@@ -0,0 +1,38 @@
+package repository;
+
+import java.util.List;
+
+import domain.Post;
+
+public class JdbcBoardRepository implements BoardRepository {
+
+    @Override
+    public void deletePostById(Long id) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void editPost(Post post) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Post getPostById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Post> getPostList() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void storePost(Post post) {
+        // TODO Auto-generated method stub
+        
+    }
+}
